2e40420aa4fd96ce0ed92af1469ed52f9c279bc7
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-03-13 18:42:45 -0700

    page configured for getting static props

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import fs from 'fs'; // can only use it inside the props otherwise an error. 
+import fs from 'fs/promises'; // can only use it inside the props otherwise an error. 
+import Link from 'next/link';
 import path from 'path';
 
 function HomePage(props){
@@ -10,23 +11,42 @@ function HomePage(props){
       <li>Product 3</li>
       <li>Product 4</li> */}
       {/* create a map for the product instead of hard code */}
-      {products.map((products) => {
-        <li key={products.id}>{products.title}</li>
-      })}
+      {products.map((product) => (
+        <li key={products.id}>
+          <Link href={`/${product.id}`}>
+          {product.title}
+        </Link>
+        </li>
+      ))}
     </ul>
   )
 }
 
 // for fethcing data 
 
-export async function getStaticProps() { // data is passed to the rest of the project through props
+export async function getStaticProps(context) { // data is passed to the rest of the project through props
+  console.log('(Re-)Generate...')
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
-  const jsonData = await fs.readFileSync(filePath, 'utf8'); // Use fs.readFileSync instead
+  const jsonData = await fs.readFile(filePath); // Use fs.readFileSync instead
   const data = JSON.parse(jsonData);
+
+  if(!data){
+    return {
+      redirect: {
+        destination: '/no-data',
+      }
+    }
+  }
+
+  if(data.products.length === 0){
+    return { notFound: true }; 
+  }
+
   return {
     props: {
-      products: data.products,
+      products: data.products
     },
+    revalidate: 10,
   };
 }
 
